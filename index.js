b319558eb3a7e75898728057c370f7b813764727
Anitha-Arjun <arjunmurugan@Arjuns-MacBook-Air.local>
2024-08-29 11:19:34 -0500

    creating instances to access the export methods

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+//default exports
+// import Stack from "./Stack.js";
+//import * as Stack from "./Stack.js"
+
 //import the class Stack from Stack.js
 import { Stack } from "./Stack.js";
 
 const stack = new Stack();
-stack.push(10);
-stack.push(20);
 stack.push(30);
-console.log(stack.peek());
-console.log(stack.pop());
+stack.push(40);
+stack.push(50);
+
+const peek = stack.peek();
+console.log(peek);
+
+const pop = stack.pop();
+console.log(pop);
 console.log(stack.peek());
 console.log(stack.isEmpty());
 console.log(stack.size());
